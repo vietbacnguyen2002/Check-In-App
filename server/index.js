7ae3b122db8d7e81172575020679ed6453a2d698
vietbac <bacnguyendev2002@gmail.com>
2025-02-28 16:44:00 +0700

    update endpoint

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors("*"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/customers",customerRoutes);
+app.use("",customerRoutes);
 
 server.listen(PORT, () => {
     connectToMongo();
